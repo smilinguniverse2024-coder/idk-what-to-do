2fa44af3dbd2b7edbec1087a95aa392262462226
smilinguniverse2024-coder <smiling.universe2024@gmail.com>
2026-06-09 15:54:59 +0200

    Testing by Alin B.

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-basic.showNumber(input.temperature())
+basic.forever(function () {
+    if (0 < input.temperature() && 15 > input.temperature()) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+    if (16 < input.temperature() && 30 > input.temperature()) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+    if (31 < input.temperature() && 50 > input.temperature()) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+})
